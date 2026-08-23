90edddf960a51ffcb76f933db35a8f618e9f90db
Umut <umutsenol79@gmail.com>
2026-08-23 20:53:21 +0300

    spline lazy import geri alindi, sayfa dinamik render olacak sekilde ayarlandi

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { SplineSceneBasic } from "@/components/spline-scene-demo"
 
 export default function Home() {
--- components/ui/splite.tsx
+++ components/ui/splite.tsx
@@ -1,8 +1,7 @@
 'use client'
 
-import dynamic from 'next/dynamic'
-
-const Spline = dynamic(() => import('@splinetool/react-spline'), { ssr: false })
+import { Suspense, lazy } from 'react'
+const Spline = lazy(() => import('@splinetool/react-spline'))
 
 interface SplineSceneProps {
   scene: string
@@ -11,9 +10,17 @@ interface SplineSceneProps {
 
 export function SplineScene({ scene, className }: SplineSceneProps) {
   return (
-    <Spline
-      scene={scene}
-      className={className}
-    />
+    <Suspense
+      fallback={
+        <div className="w-full h-full flex items-center justify-center">
+          <span className="loader"></span>
+        </div>
+      }
+    >
+      <Spline
+        scene={scene}
+        className={className}
+      />
+    </Suspense>
   )
 }
