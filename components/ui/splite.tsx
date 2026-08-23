17cbe61e584a1fc78216180a95f0bf11446ed225
Umut <umutsenol79@gmail.com>
2026-08-23 20:48:33 +0300

    spline sadece client tarafinda render edilecek sekilde duzeltildi

--- components/ui/splite.tsx
+++ components/ui/splite.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { Suspense, lazy } from 'react'
-const Spline = lazy(() => import('@splinetool/react-spline'))
+import dynamic from 'next/dynamic'
+
+const Spline = dynamic(() => import('@splinetool/react-spline'), { ssr: false })
 
 interface SplineSceneProps {
   scene: string
@@ -10,17 +11,9 @@ interface SplineSceneProps {
 
 export function SplineScene({ scene, className }: SplineSceneProps) {
   return (
-    <Suspense
-      fallback={
-        <div className="w-full h-full flex items-center justify-center">
-          <span className="loader"></span>
-        </div>
-      }
-    >
-      <Spline
-        scene={scene}
-        className={className}
-      />
-    </Suspense>
+    <Spline
+      scene={scene}
+      className={className}
+    />
   )
 }
