827e621b2efa2225a2759e4d61583ef8501a7542
Umut <umutsenol79@gmail.com>
2026-08-23 20:28:26 +0300

    ilk yukleme

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { SplineSceneBasic } from "@/components/spline-scene-demo"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center p-8">
+      <SplineSceneBasic />
+    </main>
+  )
+}
---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+      layers: true,
+    };
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: "asset/resource",
+    });
+    return config;
+  },
+};
+
+export default nextConfig;
