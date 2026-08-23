c0e90be56c944f796cd40e60f7c11152506fbeb8
Umut <umutsenol79@gmail.com>
2026-08-23 20:42:25 +0300

    spotlight fill prop hatasi duzeltildi

--- components/spline-scene-demo.tsx
+++ components/spline-scene-demo.tsx
@@ -9,7 +9,7 @@ export function SplineSceneBasic() {
     <Card className="w-full h-[500px] bg-black/[0.96] relative overflow-hidden">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="white"
+
       />
 
       <div className="flex h-full">
